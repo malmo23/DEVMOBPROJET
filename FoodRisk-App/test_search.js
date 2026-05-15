aef81ab9822d6adf96ff410eeaef7cc0a3a8f034
maldino23Y <maldanirachid2016@gmail.com>
2026-05-15 18:52:38 +0100

    chore: remove duplicate folders and old backups

--- FoodRisk-App/test_search.js
+++
@@ -1,9 +0,0 @@
-import { searchProducts } from './src/services/scannerService.js';
-
-searchProducts('nutella').then(res => {
-  console.log('Search success:', res.length);
-  process.exit(0);
-}).catch(err => {
-  console.error('Search error:', err);
-  process.exit(1);
-});
